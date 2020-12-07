a50cf7cb07bc5f9845949f5e6b20a9509b5a8b3d
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-12-06 21:56:30 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const lettersSum = require('./lettersSum');
 const removeRepeated = require('./removeRepeated');
 const replaceChar = require('./replaceChar');
 
-console.log(lettersSum('ab', 'cd'))
-console.log(removeRepeated('xxxzzzwww', 2))
-console.log(replaceChar('?te?s?t', 2))
-
 /*
+ * console.log(lettersSum('ab', 'cd'))
+ * console.log(removeRepeated('xxxzzzwww', 2))
+ * console.log(replaceChar('?te?s?t', 2))
+ * 
  * You can run console.log() to test the functions here.
- */
+*/
