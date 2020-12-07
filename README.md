1075b7f4cbf2ed2a58fff93a28ffd2c78244d9d4
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-12-06 21:52:44 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # challenge-aurum
- 
+
+This challenge consists of three parts. The first is to create a function that is capable of removing repeated characters in a string according to a limit. The second part is to create a function that replace the character '?' for what comes before him according to a limit. The third part is to create a function that does a calculation with the letters of the alphabet.
--- src/lettersSum.js
+++ src/lettersSum.js
@@ -1,10 +1,29 @@
 const lettersSum = (string1, string2) => {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const obj = {...alphabet};
+  const array = [...'abcdefghijklmnopqrstuvwxyz'];
 
-  console.log(Object.keys(obj))
-  // return (maiorLetraString1 menorLetraString2 ) + (maiorLetraString2  menorLetraString1)
+  const getChar = (string, index) => {
+    const stringArr = [...string];
+    const char = stringArr.sort()[index];
+
+    return array.indexOf(char) + 1;
+  }
+
+  const firstNumbers = (string) => {
+    return getChar(string, 0);
+  }
+
+  const lastNumbers = (string) => {
+    return getChar(string, string.length - 1);
+  }
+
+  const firstString1 = firstNumbers(string1);
+  const lastString1 = lastNumbers(string1);
+  const firstString2 = firstNumbers(string2);
+  const lastString2 = lastNumbers(string2);
+
+  const calc = (lastString1 * firstString2) + (firstString1 * lastString2);
+
+  return calc;
 }
 
-lettersSum()
-/* export default lettersSum; */
+export default lettersSum; 
